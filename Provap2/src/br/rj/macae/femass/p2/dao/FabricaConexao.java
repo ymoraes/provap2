bbe90ffcc81fb58c225ad95b727b9ca948a60d02
YMoraes <YMoraes@DESKTOP-0E7FUQ3>
2017-12-06 12:45:48 -0200

    Updates

---
+++ Provap2/src/br/rj/macae/femass/p2/dao/FabricaConexao.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.rj.macae.femass.p2.dao;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+
+public class FabricaConexao {
+    public static EntityManagerFactory getConexao(){
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("provap2PU");
+        return emf;
+
+    }
+}
---
+++ Provap2/src/br/rj/macae/femass/p2/dao/IDAO.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.rj.macae.femass.p2.dao;
+
+import java.sql.SQLException;
+import java.util.List;
+
+/**
+ *
+ * @author anamm
+ */
+public interface IDAO {
+    public void cadastrar(Object o) throws SQLException;
+    public void alterar(Object o) throws SQLException;
+    public void excluir(Object o) throws SQLException;
+    public List listarTodos() throws SQLException;
+    public Object listarPorId(int id) throws SQLException;
+}
